f6e0eed451898c8ff68bb6cfe2c722d127ce9155
bbangwon <bbangwon.chung@gmail.com>
2024-02-22 00:47:53 +0900

    진동을 위한 Mathf.Sin

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class Oscillator : MonoBehaviour
 {
-    Vector3 startingPosition;
     [SerializeField] 
     Vector3 movementVector;
 
     [SerializeField]
-    [Range(0, 1)]
-    float movementFactor;
+    float period = 2f;
 
+    Vector3 startingPosition;
+    float movementFactor;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +21,13 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period;
+
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
